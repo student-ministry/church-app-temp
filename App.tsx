21a13ac9918426e22b7fee7cf776b7c7a2aad07c
youpeng <youpeng@youpengs-MacBook-Air.local>
2020-02-08 23:44:56 -0600

    init expo typescript react

--- App.tsx
+++ App.tsx
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Hello, world!!!</Text>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
